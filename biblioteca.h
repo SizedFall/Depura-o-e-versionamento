017773cb64de7cf217088e4e267090d7abb20491
yohannd1 <mitonanan12@gmail.com>
2024-12-17 21:39:57 -0300

    adicionando biblioteca (.h) e código para conversão de tempo

---
+++ biblioteca.h
@@ -0,0 +1,8 @@
+double segundosParaMinutos(double seg);
+double segundosParaHoras(double seg);
+
+double minutosParaSegundos(double min);
+double minutosParaHoras(double min);
+
+double horasParaMinutos(double min);
+double horasParaSegundos(double min);
