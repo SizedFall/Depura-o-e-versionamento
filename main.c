051dfbe97fd787a7c6b27fef97b81651c41c2b1d
VitorPereiraFontes <vitor.fontes002@gmail.com>
2024-12-19 20:04:23 -0300

    Adicionando definição de funçoes
    Adicionando as definições das funções utilizadas na conversão de bytes.

--- main.c
+++ main.c
@@ -1,6 +1,38 @@
 // Importações
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
 // Definições das funções
+
+/// Conversão de bytes
+
+double bytesParaKilobytes(double bytes);
+double bytesParaMegabytes(double bytes);
+double bytesParaGigabytes(double bytes);
+double bytesParaTerabytes(double bytes);
+double kilobytesParaBytes(double kilobytes);
+double kilobytesParaMegabytes(double kilobytes);
+double kilobytesParaGigabytes(double kilobytes);
+double kilobytesParaTerabytes(double kilobytes);
+double megabytesParaBytes(double megabytes);
+double megabytesParaKilobytes(double megabytes);
+double megabytesParaGigabytes(double megabytes);
+double megabytesParaTerabytes(double megabytes);
+double gigabytesParaBytes(double gigabytes);
+double gigabytesParaKilobytes(double gigabytes);
+double gigabytesParaMegabytes(double gigabytes);
+double gigabytesParaTerabytes(double gigabytes);
+double terabytesParaBytes(double terabytes);
+double terabytesParaKilobytes(double terabytes);
+double terabytesParaMegabytes(double terabytes);
+double terabytesParaGigabytes(double terabytes);
+void imprimirOpcoes(char *opcoes[], int tamanhoArray, int indexDaOpcaoIgnorada);
+bool opcaoValida(int opcao, int opcoesValidas[], int tamanhoAray);
+void removerDasOpcoesPorIndex(int opcoes[], int *tamanho, int index);
+int obterInputDoUsuario(int opcoesValidas[],int tamanho);
 
 int main(int argc, char const *argv[])
 {
