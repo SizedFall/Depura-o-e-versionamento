6c033fe7ba93f26d8d76d5c7f11b72324fdd8bcb
yohannd1 <mitonanan12@gmail.com>
2024-12-22 01:52:09 -0300

    movendo interface principal para main.c

--- main.c
+++ main.c
@@ -19,9 +19,51 @@ void menuConversaoTempo(void);
 void menuConversaoComprimento(void);
 double menuConversaoMassa(void);
 
-int main(int argc, char const *argv[])
+int main(void)
 {
-    return 0;
+    int opcao, continuar = 1;
+
+    printf("Bem vindo ao menu de conversoes! \n");
+    printf("----------------------------------- \n");
+
+    while (continuar == 1)
+	{
+		printf("Qual conversao deseja realizar? \n");
+		printf("1- Unidades de distancia \n");
+		printf("2- Unidades de tempo \n");
+		printf("3- Unidades de potencia \n");
+		printf("4- Unidades de temperatura \n");
+		printf("5- Unidades de massa \n");
+		printf("6- Sair \n");
+		scanf("%d", &opcao);
+
+		switch (opcao)
+		{
+			case 1: // Chamar a função responsável pela conversão das unidades de distancia
+				break;
+
+			case 2: // Chamar a função responsável pela conversão das unidades de tempo
+				menuConversaoTempo();
+				break;
+
+			case 3: // Chamar a função responsável pela conversão das unidades de potência
+				break;
+
+			case 4: // Chamar a função responsável pela conversão das unidades de temperatura
+				break;
+
+			case 5: // Chamar a função responsável pela conversão das unidades de massa
+				menuConversaoMassa();
+				break;
+
+			case 6: // Sair do menu e encerrar o programa
+				continuar = 0;
+				printf("6 foi escolhido, saindo do programa \n");
+				break;
+		}
+    }
+
+	return 0;
 }
 
 // Implementação das funções
